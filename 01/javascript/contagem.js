0ba38c71ccd20db17886084d900d39281f368f8f
aliciasouzan <aliciasouzan@outlook.com>
2023-03-23 13:35:30 -0300

    01

--- 01/javascript/contagem.js
+++ 01/javascript/contagem.js
@@ -1,9 +1,12 @@
-function count(numbers, N) {
+function contagem(array, n) {
   let count = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] >= 1 && numbers[i] <= N) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= 1 && array[i] <= n) {
       count++;
     }
   }
   return count;
 }
+
+console.log(contagem([1, 2, 3, 4, 5], 3));
+console.log(contagem([-1, 0, 1, 2, 3], 2));
--- 01/javascript/fibonacci.js
+++ 01/javascript/fibonacci.js
@@ -1,9 +1,13 @@
 function fibonacci(n) {
-  if (n <= 0) return [];
-  if (n === 1) return [0];
-  const fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
   }
-  return fib;
+  return b;
 }
+
+console.log(fibonacci(7));
+console.log(fibonacci(12));
